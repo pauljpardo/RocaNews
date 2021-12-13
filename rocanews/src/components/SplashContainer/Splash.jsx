f51881c07e165c93915e16658cdd8e0c09745e9a
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-12-13 16:30:26 -0500

    Splash styling

--- rocanews/src/components/SplashContainer/Splash.jsx
+++ rocanews/src/components/SplashContainer/Splash.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './style.css'
 
 export default function SplashContainer() {
   return (
@@ -7,15 +7,32 @@ export default function SplashContainer() {
       <div className='main_div'>
         
         <div className='left_panel'>
-          <p>We live in an unprecedented time, where finding trustworthy
+          <p> We live in an unprecedented time, where finding trustworthy
             news can be as hard as climbing Mount Everest.
             RocaNews was established in order to provide our readers with
             Unbiased and Reliable articles in which one can learn recent
             significant stories, told in a way that news should be, Purely informative.</p>
         </div>
+
         <div className='right_panel'>
-          
+          <div>
+            <p className='recent'> Recent Stories and News</p>
+              <div className= 'story-list'>
+              <ol>
+                <li> <p>The Ghislaine Trial - a Roca wrap on Ghislaine Maxwell, whose high-profile trial began this morning. Part 1 of 2 </p> <img src='https://i.imgur.com/WmEzaFn.png'></img></li>
+                <hr />
+                <li><p>Virgil’s Fashion - a Roca wrap on Virgil Abloh, who died Sunday </p><img src='https://i.imgur.com/7S4B1SX.png'/></li>
+                <hr />
+                <li><p>The WTA pulls out of China, the MLB enters a lockout, & more - quick cards for December 3 </p><img src='https://i.imgur.com/pO6i1lT.png'/></li>
+                <hr />
+                <li><p>The Ghislaine Trial - a Roca wrap on Ghislaine Maxwell, whose high-profile trial began yesterday. Part 2 of 2 </p><img src='https://i.imgur.com/bdBPG6L.png'/> </li>
+                <hr />
+                <li><p>30,000 people trapped in Disneyland, abortion hearings, & more - quick cards for November 2 </p><img src='https://i.imgur.com/kQZKqoK.png'/></li>
+              </ol>
+              </div>
+          </div>
         </div>
+
       </div>
     </>
   )
--- rocanews/src/layouts/Layout.jsx
+++ rocanews/src/layouts/Layout.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import './Layout.css'
 
 export default function Layout(props) {
   return (
     <>
       <div>
       <header>
-          <h1><b>Roca</b>News</h1>
+          <h1>RocaNews</h1>
           <p><i>Unbiased. Informative. Trustworthy.</i></p>
-        </header>
-        <hr />
-        <hr />
+      </header>
+        <hr/>
       </div>
       <div className="layout-children">{props.children}</div>
     </>
