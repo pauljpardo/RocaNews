a596615045760401e068dc7a724af0b998bebb56
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-12-12 20:26:33 -0500

    backend get functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Story.destroy_all
+Comment.destroy_all
+
+@admin = Story.create!(img_url1: 'https://i.imgur.com/4WZpZvc.png', img_url2: 'https://i.imgur.com/6a2iGUR.png', img_url3: 'https://i.imgur.com/B8lrATl.png', img_url4: 'https://i.imgur.com/8mFVGny.png', img_url5: 'https://i.imgur.com/Wkh0dnu.png', title: 'A goodbye, a growing boycott, & more - quick cards for December 9 🌊' )
+
+@admin = Comment.create!(name: 'paul', text: 'test')
