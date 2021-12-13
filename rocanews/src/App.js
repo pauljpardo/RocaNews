97c7ed0842b32897defe160e76384ece47a4758c
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-12-13 11:02:52 -0500

    layout.jsx

--- rocanews/src/App.js
+++ rocanews/src/App.js
@@ -1,9 +1,10 @@
+import Layout from "./layouts/Layout";
 
 
 function App() {
   return (
     <>
-      
+      <Layout />
     </>
   );
 }
---
+++ rocanews/src/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Layout() {
+  return (
+    <>
+      <div>
+      <header>
+          <h1><b>Roca</b>News</h1>
+          <p><i>Unbiased. Informative. Trustworthy.</i></p>
+        </header>
+        <hr />
+        <hr />
+      </div>
+    </>
+  )
+}
